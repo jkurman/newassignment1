f34b0c77ff9e9cfdade046ab4b4dbb71719014b2
Jonathan Chiu <jayjaychiu@Hotmail.com>
2015-10-29 23:36:28 +0000

    final commit

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,28 +1,47 @@
-var app = angular.module('simple-chart', []);
+//Reference for Google-Chart:   https://developers.google.com/chart/interactive/docs/gallery/bubblechart
+var app = angular.module('splashchart', []);
 google.load("visualization", "1", {packages:["corechart"]});
-google.setOnLoadCallback(function() {
+
+// app.controller('MainController', ['$scope', '$http',  function($scope, $http) {
+//   $http.get('/').success(function(data){
+    
+//   var dataArray = formatDataForView(data);
+//   var table = google.visualization.arrayToDataTable(dataArray, false);
+  
+//   var options = {
+//      title: 'SplashBrothers vs Top 10 Three Point Shooters',
+//      hAxis: {title: 'Threes Made'},
+//      vAxis: {title: 'Threes Attempted'},
+//      bubble: {textStyle: {fontSize: 11}}
+//       //colors: ['#1b9e77', '#d95f02', '#7570b3', '#db9500']
+//      };
+//     var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
+//     chart.draw(data, options);
+
+//   });
+// }]);
 
 app.controller('MainController', ['$scope', '$http',  function($scope, $http) {
-  $http.get('/').success(function(data){
+  $http.get('/data').success(function(data){
     
-  var dataArray = formatDataForView(data);
+    var dataArray = formatDataForView(data);
   
-  dataArray.splice(1,1);
-
-  var table = google.visualization.arrayToDataTable(dataArray, false);
-  
-   var options = {
-      title: 'Ball Stuff',
-      hAxis: {title: 'Threes Made'},
-      vAxis: {title: 'Threes Attempted'},
-      bubble: {textStyle: {fontSize: 11}}
-      //colors: ['#1b9e77', '#d95f02', '#7570b3', '#db9500']
-      };
-  var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-  chart.draw(table, options);
-
+    var table = google.visualization.arrayToDataTable(dataArray, false);
+    var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
+    
+    var options = {
+     title: 'SplashBrothers vs Top 10 Three Point Shooters',
+     hAxis: {title: 'Threes Made'},
+     vAxis: {title: 'Threes Attempted'},
+     bubble: {textStyle: {fontSize: 11}
+     }
+    };
+    
+    chart.draw(table, options);
   });
 }]);
+
+
 
 function formatDataForView(data) {
   
@@ -39,11 +58,16 @@ function formatDataForView(data) {
     data.forEach(function(value){
         var dataEntry = [];
         for(var prop in value) {
-          dataEntry.push(parseInt(value[prop], 0));
+            if(prop == "id") {
+             dataEntry.push(value[prop]);   
+            } else {
+             dataEntry.push(parseFloat(value[prop])); 
+            }
           }
         
         dataArray.push(dataEntry);
     });
   
+    //console.table(dataArray);
     return dataArray;
 }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var graph=mongoose.model('GraphData');
+var graph = mongoose.model('GraphData');
 
 router.get('/', function(req, res, next) {
-  graph.find(function(err, posts){
-    if(err){ return next(err); }
+  res.render('index', { title: 'Express' });
+});
 
-    res.json(posts);
+router.get('/data', function(req, res, next) {
+  graph.find({}, {_id: 0}, function(err, data){
+  if(err){ return next(err); }
+  res.json(data);
   });
 });
 
